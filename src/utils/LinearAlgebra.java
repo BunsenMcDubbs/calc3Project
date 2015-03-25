fc2f571b3485c350be06f6af20529af457e8fcd3
Andrew Dai <theman@andrewdai.co>
2015-03-24 20:56:01 -0400

    moved methods over

--- src/utils/LinearAlgebra.java
+++ src/utils/LinearAlgebra.java
@@ -116,44 +116,4 @@ public class LinearAlgebra {
         return new Vector(sum);
     }
 
-    /**
-     * Finds the LU factorization and error of a given matrix
-     * @param m matrix to LU factorize
-     * @return a Result object where the first Matrix (a) is L
-     * and the second (b) is U
-     */
-    public static Result lu_fact(Matrix m) {
-        return null;
-    }
-
-    /**
-     * Finds the QR factorization and error of a given matrix
-     * with the Householders method.
-     * @param m matrix to QR factorize
-     * @return a Result object where the first Matrix (a) is Q
-     * and the second (B) is R
-     */
-    public static Result qr_fact_househ(Matrix m) {
-        return null;
-    }
-
-    /**
-     * Finds the QR factorization and error of a given matrix
-     * with the Givens Rotation method.
-     * @param m matrix to QR factorize
-     * @return a Result object where the first Matrix (a) is Q
-     * and the second (B) is R
-     */
-    public static Result qr_fact_givens(Matrix m) {
-        return null;
-    }
-
-    public static Vector solve_lu_b(Matrix m, Vector v) {
-        return null;
-    }
-
-    public static Vector solve_qr_b(Matrix m, Vector v) {
-        return null;
-    }
-
 }
