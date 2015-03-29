b92167ee4bee31b1cf9a32fc9cc7805c138e2e0a
William Greenleaf <Orangelink0@gmail.com>
2015-03-28 20:04:31 -0400

    Added more documentation to PowerResult

--- src/utils/PowerResult.java
+++ src/utils/PowerResult.java
@@ -1,21 +1,35 @@
 package utils;
 /**
  * William Greenleaf, 3/24/2015
- * @version 0.1
+ * Represents the result of the power method.
+ * @version 1
  */
 public class PowerResult {
     private Vector eiVect;
     private double eiNum;
     
+    /**
+     * Creates a power result.
+     * @param eiVect eigen vector
+     * @param eiNum eigen number
+     */
     public PowerResult(Vector eiVect, double eiNum) {
         this.eiVect = eiVect;
         this.eiNum = eiNum;
     }
     
+    /**
+     * Retrieves eigen vector
+     * @return eigen vector
+     */
     public Vector getEigenVector() {
         return eiVect;
     }
     
+    /**
+     * Retrieves eigen value
+     * @return eigen value
+     */
     public double getEigenValue() {
         return eiNum;
     }
