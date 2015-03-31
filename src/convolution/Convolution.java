e638030b3567602d853be23c4c718222577f3fff
Andrew Dai <theman@andrewdai.co>
2015-03-30 22:40:36 -0400

    gauss seidel

--- src/convolution/Convolution.java
+++ src/convolution/Convolution.java
@@ -8,7 +8,7 @@ import java.util.Random;
 public class Convolution {
 
     static Random r = new Random();
-    static final int MAX_ITERATIONS = 500;
+    static final int MAX_ITERATIONS = 2000;
 
     /**
      * Performs Jacobi iteration to solve for vector x in Ax = y on
@@ -57,7 +57,6 @@ public class Convolution {
         int count = 0; // number of iterations performed
         do {
             if (count > MAX_ITERATIONS) {
-                System.out.println(count);
                 throw new Exception("Unable to approach convergence " +
                         "before exceeding the maximum number of iterations" +
                         " (" + MAX_ITERATIONS + ")");
@@ -77,6 +76,93 @@ public class Convolution {
         } while (diffNormSq > tolSq);
         return new BitMatrixAndCount(next, count);
     }
+
+    /**
+     *
+     * @param a Matrix A should be a square matrix (invertible)
+     * @param b
+     * @param x0
+     * @param tol
+     * @return
+     * @throws Exception
+     */
+    public static BitMatrixAndCount gauss_seidel(BitMatrix a, BitMatrix b,
+                                                 BitMatrix x0, double tol)
+            throws Exception{
+
+        double tolSq = tol * tol;
+        // Copy x0 into xRaw
+        boolean[] xRaw = new boolean[x0.getRows()];
+        for (int i = 0; i < xRaw.length; i++) {
+            xRaw[i] = x0.getBool(i, 0);
+        }
+        int count = 0;
+        int diffNormSq;
+        do {
+            if (count > MAX_ITERATIONS) {
+                throw new Exception("Unable to approach convergence " +
+                        "before exceeding the maximum number of iterations" +
+                        " (" + MAX_ITERATIONS + ")");
+            }
+            diffNormSq = 0;
+            for (int i = 0; i < xRaw.length; i++) {
+                boolean xiNext = b.getBool(i, 0);
+                for (int j = 0; j < xRaw.length; j++) {
+                    if (i != j) {
+                        xiNext = xiNext != (xRaw[j] && a.getBool(i, j));
+                    }
+                }
+                if (xiNext != xRaw[i]) {
+                    diffNormSq++;
+                }
+                xRaw[i] = xiNext;
+            }
+            count++;
+        } while (diffNormSq > tolSq);
+        return new BitMatrixAndCount(new BitMatrix(xRaw), count);
+    }
+
+    /**
+     * Finds the inverse of a matrix with forwards substitution
+     * @param mat given matrix (lower triangular)
+     * @return inverse of the given matrix
+     */
+    public static BitMatrix inverseDown(BitMatrix mat) {
+        if (mat.getRows() != mat.getCols()) {
+            System.out.println("Matrix must be square to be invertible");
+            return null;
+        }
+        boolean[][] inverse = new boolean[mat.getRows()][mat.getRows()];
+        boolean[][] original = new boolean[mat.getRows()][mat.getRows()];
+
+        // Copy matrix mat to original
+        for (int row = 0; row < mat.getRows(); row++) {
+            inverse[row][row] = true; // start with inverse as identity matrix
+            for (int col = 0; col < mat.getCols(); col++) {
+                original[row][col] = mat.getBool(row, col);
+            }
+        }
+        // Goes through all the rows (i is the current-pivot-row)
+        for (int i = 0; i < original.length; i++) {
+            // Assume pivots are already 1 because else... this is hard
+            // Goes through all the rows underneath the "i'th" row (j -> row)
+            for (int j = i + 1; j < original.length; j++) {
+                // Row reduce for original and copy operations to inverse
+                // Row reduction (k - column)
+                // stop if original[j][i] (element below pivot) is false/0
+                // because no substitution is needed
+                for (int k = 0; original[j][i] && k < original[i].length; k++) {
+                    // != (XOR) is sort of like ADDITION/SUBTRACTION
+                    original[j][k] = original[j][k] != original[i][k];
+                    inverse[j][k] = inverse[j][k] != inverse[i][k];
+                }
+            }
+            // Don't need to row reduce inverse because pivots are all 1
+        }
+
+        return new BitMatrix(inverse);
+    }
+
 
     public static BitMatrix getRandomVector(int length) {
         boolean[] v = new boolean[length];
@@ -105,19 +191,32 @@ public class Convolution {
     }
 
     public static BitMatrix getA0(int length) {
-        return getAHelper(length, BitMatrix.intsToBoolsVecRaw(1, 0, 1, 1));
+        return getAHelper(length, BitMatrix.intsToBoolsVecRaw(1, 0, 1, 1), false);
     }
 
     public static BitMatrix getA1(int length) {
-        return getAHelper(length, BitMatrix.intsToBoolsVecRaw(1, 1, 0, 1));
+        return getAHelper(length, BitMatrix.intsToBoolsVecRaw(1, 1, 0, 1), false);
     }
 
-    private static BitMatrix getAHelper(int length, boolean[] row) {
-        boolean[][] id = BitMatrix.identityRaw(length + 3, length);
-        for (int i = 0; i < id.length; i++) {
-            int j = i < length ? i : length - 1;
-            for (; j > i - row.length && j >= 0; j--) {
-                id[i][j] = id[i][j] || row[i - j];
+    public static BitMatrix getA0Square(int length) {
+        return getAHelper(length, BitMatrix.intsToBoolsVecRaw(1, 0, 1, 1), true);
+    }
+
+    public static BitMatrix getA1Square(int length) {
+        return getAHelper(length, BitMatrix.intsToBoolsVecRaw(1, 1, 0, 1), true);
+    }
+
+
+    private static BitMatrix getAHelper(int length, boolean[] row,
+                                        boolean square) {
+
+        int height = length + row.length - 1;
+        boolean[][] id = BitMatrix.identityRaw(height,
+                square ? height : length);
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < row.length && i - j >= 0; j++) {
+                id[i][i - j] = row[j];
             }
         }
         return new BitMatrix(id);
--- src/convolution/ConvolutionTest.java
+++ src/convolution/ConvolutionTest.java
@@ -1,14 +1,19 @@
 package convolution;
 
+import hilbert.Hilbert;
+
 /**
  * Created by andrew on 3/29/15.
  */
 public class ConvolutionTest {
 
     public static void main(String... args) {
-        findXWithY0ExampleJacobi();
-        findXWithY1ExampleJacobi();
-        testJacobiRandom(12);
+//        findXWithY0ExampleJacobi();
+//        findXWithY1ExampleJacobi();
+//        testJacobiRandom(20);
+//        testInverseDown();
+//        findXWithY1ExampleGaussSeidel();
+        testGaussSeidelRandom(100);
     }
 
     public static void randomVecToString() {
@@ -49,10 +54,11 @@ public class ConvolutionTest {
     }
 
     public static void findXWithY1ExampleJacobi() {
-        BitMatrix x0 = BitMatrix.intsToBoolsVec(1, 0, 0, 0, 0, 0, 0, 0);
+        BitMatrix x0 = BitMatrix.intsToBoolsVec(0, 0, 0, 0, 0, 0, 0, 0);
         BitMatrix x = BitMatrix.intsToBoolsVec(1, 0, 1, 1, 0);
         BitMatrix y1 = Convolution.findy1(x);
         BitMatrix a1 = Convolution.getA1(x.getRows());
+        System.out.println("A1\n" + a1);
         try {
             System.out.println(Convolution.jacobi(a1, y1, x0, 0));
         } catch (Exception e) {
@@ -95,8 +101,76 @@ public class ConvolutionTest {
         }
 
         System.out.println("This is x (again): " + x);
+    }
+
+    public static void testGaussSeidelRandom(int length) {
+        System.out.printf("Gauss Seidel decoding with %d length input%n", length);
+        BitMatrix x = Convolution.getRandomVector(length);
+        System.out.println("This is x: " + x);
+        System.out.println("\nEncoding");
+        // Encode x -> y0
+        BitMatrix y0 = Convolution.findy0(x);
+        System.out.println("This is y0: " + y0);
+        // Encode y -> y1
+        BitMatrix y1 = Convolution.findy1(x);
+        System.out.println("This is y1: " + y1);
+
+        // find A0 and A1
+        BitMatrix a0 = Convolution.getA0(length);
+        BitMatrix a1 = Convolution.getA1(length);
+
+        // x0 (first guess)
+        BitMatrix x0 = new BitMatrix(new boolean[length + 3]);
+        System.out.println("\nDecoding");
+        try {
+            // Decode y0 -> x
+            BitMatrixAndCount y0x = Convolution.gauss_seidel(a0, y0, x0, 0);
+            System.out.println("This is x from y0: " + y0x);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            // Decode y1 ->
+            BitMatrixAndCount y1x = Convolution.gauss_seidel(a1, y1, x0, 0);
+            System.out.println("This is x from y1: " + y1x);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("This is x (again): " + x);
+    }
+
+    public static void findXWithY1ExampleGaussSeidel() {
+        BitMatrix x0 = BitMatrix.intsToBoolsVec(1, 1, 0, 1, 1, 1, 1, 1);
+        BitMatrix x = BitMatrix.intsToBoolsVec(1, 0, 1, 1, 0);
+        BitMatrix y1 = Convolution.findy1(x);
+        BitMatrix a1 = Convolution.getA1Square(x.getRows());
+        System.out.println(x);
+        try {
+            System.out.println(Convolution.gauss_seidel(a1, y1, x0, 0));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void testInverseDown() {
+        System.out.println("Square, predefined A0 for a length 5 input (8x8)");
+        BitMatrix a0 = Convolution.getA0Square(5);
+        System.out.println(a0);
+        System.out.println("Its inverse");
+        BitMatrix a0Inv = Convolution.inverseDown(a0);
+        System.out.println(a0Inv);
+        System.out.println("A0 * A0Inv = I?");
+        System.out.println(BitMatrix.matrixMultiply(a0, a0Inv));
 
 
+        System.out.println("Square, predefined A1 for a length 5 input (8x8)");
+        BitMatrix a1 = Convolution.getA1Square(5);
+        System.out.println(a1);
+        BitMatrix a1Inv = Convolution.inverseDown(a1);
+        System.out.println(a1Inv);
+        System.out.println("A0 * A0Inv = I?");
+        System.out.println(BitMatrix.matrixMultiply(a1, a1Inv));
     }
 
 }
